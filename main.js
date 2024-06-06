57ef119da7a6f63a7ea731fdb000a3a288448969
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-06-06 18:54:27 +0600

    footer completed

--- main.js
+++ main.js
@@ -50,3 +50,13 @@ themeBtn.addEventListener('click', (e)=>{
     }
 
 });
+
+
+// footer 
+
+document.getElementById('copy-right').textContent = `
+\u00A9 ${new Date().getFullYear()} all the rights reserved.
+`
+document.getElementById('credit').textContent = `
+designed by md rakibul hasan.
+`
